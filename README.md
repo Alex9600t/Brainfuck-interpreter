968156159f70744106490b9c5cfcac424752e39a
Alex9600t <dukovaleks2@gmail.com>
2025-02-08 02:21:07 +0300

    feat(liveInterpreter)

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
 # Brainfuck Interpreter
 ## Интерпретатор написанный на C++ для brainfuck
-### Использование brainfuck.exe:
-```bash
-brainfuck file.bf
+
+## Установка:
+1. Скопируйте репозиторий:
+```sh
+git clone https://github.com/dimabreus/Brainfuck-interpreter.git
 ```
-Или
-```bash
+2. Скомпилируйте программу:
+```sh
+g++ main.cpp -o brainFuck
+```
+> [!NOTE]
+> Если вы компилируйте программу для ОС Windows, то вы должны добавить флаг `-static`
+
+### Запуск программы:
+
+## Режим первый (Мгновенный интепретатор):
+```sh
+./brainfuck
+```
+```sh
+File not specified. Instant interpreter mode enabled.
+(Enter BrainFuck code)
+(Ctrl+D to interpret and exit):
+
+> (Сюды код)
+```
+> [!IMPORTANT]
+> Для интепретации надо нажать Enter, а потом Ctrl+D
+
+## Режим второй (Интепретация из файла .bf):
+```sh
+./brainfuck <FileName .bf>
+```
+Пример:
+```sh
 ./brainfuck file.bf
 ```
--- main.cpp
+++ main.cpp
@@ -2,27 +2,30 @@
 #include <fstream>
 #include <vector>
 #include <stack>
+#include <iterator>
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " file.bf" << std::endl;
-        return 1;
-    }
+    std::vector<char> code;
 
-    std::ifstream file(argv[1]);
-    if (!file.is_open()) {
-        std::cerr << "Error: Could not open file " << argv[1] << std::endl;
-        return 1;
+    if (argc == 2) {
+        std::ifstream file(argv[1]);
+        if (!file.is_open()) {
+            std::cerr << "Error: Could not open file " << argv[1] << std::endl;
+            return 1;
+        }
+	code.assign(std::istreambuf_iterator<char>(file),
+                    std::istreambuf_iterator<char>());
+        file.close();
+    } else {
+        std::cout << "File not specified. Instant interpreter mode enabled. \n(Enter BrainFuck code) \n(Ctrl+D to interpret and exit):" << std::endl;
+        code.assign(std::istreambuf_iterator<char>(std::cin),
+                    std::istreambuf_iterator<char>());
     }
-
-    std::vector<char> code((std::istreambuf_iterator<char>(file)),
-                            std::istreambuf_iterator<char>());
-    file.close();
 
     std::vector<unsigned char> tape(30000, 0);
     size_t ptr = 0;
-
     std::stack<size_t> loops;
+
     for (size_t pc = 0; pc < code.size(); ++pc) {
         switch (code[pc]) {
             case '>':
@@ -48,6 +51,10 @@ int main(int argc, char* argv[]) {
                     size_t depth = 1;
                     while (depth > 0) {
                         ++pc;
+                        if (pc >= code.size()) {
+			    std::cerr << "Error: Inconsistency '[' and ']'" << std::endl;	
+                            return 1;
+                        }
                         if (code[pc] == '[') ++depth;
                         if (code[pc] == ']') --depth;
                     }
@@ -57,13 +64,20 @@ int main(int argc, char* argv[]) {
                 break;
             case ']':
                 if (tape[ptr] != 0) {
+                    if (loops.empty()) {
+                        std::cerr << "Error: Inconsistency ']' and '['" << std::endl;
+                        return 1;
+                    }
                     pc = loops.top();
                 } else {
                     loops.pop();
                 }
                 break;
+            default:
+		break;
         }
     }
 
     return 0;
 }
+
